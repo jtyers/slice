7fe8e7b5c71e597bf741a20bae79ef164b6cdaf3
jtyers <jtyers@gmail.com>
2020-05-26 21:14:27 +0100

    add test for custom types

---
+++ custom_type_test.go
@@ -0,0 +1,5 @@
+package main
+
+type CustomType struct {
+	name string
+}
--- go-dash_test.go
+++ go-dash_test.go
@@ -2,6 +2,7 @@ package main
 
 //go:generate ./slice -out go-dash_generated_test.go -package main -type string -dir .
 //go:generate ./slice -out go-dash_generated_ptr_test.go -package main -type *string -dir .
+//go:generate ./slice -out go-dash_generated_custom_test.go -package main -type CustomType -import github.com/jtyers/go-dash-slice -dir .
 
 import (
 	"strings"
@@ -511,6 +512,257 @@ func TestStringPtrUniq(t *testing.T) {
 
 	for _, test := range tests {
 		c := NewStringPtrSlice(test.input)
+
+		got := c.Uniq()
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func ct(name string) CustomType {
+	return CustomType{name}
+}
+
+func TestCustomTypeConcat(t *testing.T) {
+	var tests = []struct {
+		name   string
+		input  []CustomType
+		add    []CustomType
+		output []CustomType
+	}{
+		{
+			"should concat a new item",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			[]CustomType{ct("fourth"), ct("fifth")},
+			[]CustomType{ct("first"), ct("second"), ct("third"), ct("fourth"), ct("fifth")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
+
+		got := c.Concat(test.add)
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func TestCustomTypeDrop(t *testing.T) {
+	var tests = []struct {
+		name   string
+		input  []CustomType
+		i      int
+		output []CustomType
+	}{
+		{
+			"should drop items from the left",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			2,
+			[]CustomType{ct("third")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
+
+		got := c.Drop(test.i)
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func TestCustomTypeDropRight(t *testing.T) {
+	var tests = []struct {
+		name   string
+		input  []CustomType
+		i      int
+		output []CustomType
+	}{
+		{
+			"should drop items from the right",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			2,
+			[]CustomType{ct("first")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
+
+		got := c.DropRight(test.i)
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func TestCustomTypeFilter(t *testing.T) {
+	var tests = []struct {
+		name   string
+		input  []CustomType
+		f      func(CustomType, int) bool
+		output []CustomType
+	}{
+		{
+			"should run items via filter function",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			func(s CustomType, i int) bool {
+				return s.name[len(s.name)-1:len(s.name)] == "d"
+			},
+			[]CustomType{ct("second"), ct("third")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
+
+		got := c.Filter(test.f)
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func TestCustomTypeFirst(t *testing.T) {
+	var tests = []struct {
+		name   string
+		input  []CustomType
+		output []CustomType
+	}{
+		{
+			"should return first item",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			[]CustomType{ct("first")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
+
+		got := c.First()
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func TestCustomTypeLast(t *testing.T) {
+	var tests = []struct {
+		name   string
+		input  []CustomType
+		output []CustomType
+	}{
+		{
+			"should return last item",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			[]CustomType{ct("third")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
+
+		got := c.Last()
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func TestCustomTypeMap(t *testing.T) {
+	var tests = []struct {
+		name    string
+		input   []CustomType
+		mapFunc func(CustomType, int) CustomType
+		output  []CustomType
+	}{
+		{
+			"should map input to output for simple function",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			func(s CustomType, i int) CustomType {
+				result := strings.ToUpper(s.name)
+				return CustomType{result}
+			},
+			[]CustomType{ct("FIRST"), ct("SECOND"), ct("THIRD")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
+
+		got := c.Map(test.mapFunc)
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func TestCustomTypeReduce(t *testing.T) {
+	var tests = []struct {
+		name    string
+		input   []CustomType
+		initial CustomType
+		f       func(CustomType, CustomType, int) CustomType
+		output  []CustomType
+	}{
+		{
+			"should reduce input to output via reduce function",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			ct("initial"),
+			func(acc CustomType, val CustomType, i int) CustomType {
+				vv := acc.name + "-" + strings.ToUpper(val.name)
+				return ct(vv)
+			},
+			[]CustomType{ct("initial-FIRST-SECOND-THIRD")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
+
+		got := c.Reduce(test.f, test.initial)
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func TestCustomTypeReverse(t *testing.T) {
+	var tests = []struct {
+		name   string
+		input  []CustomType
+		output []CustomType
+	}{
+		{
+			"should reverse slice order",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			[]CustomType{ct("third"), ct("second"), ct("first")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
+
+		got := c.Reverse()
+
+		require.Equal(t, got.Value(), test.output)
+	}
+}
+
+func TestCustomTypeUniq(t *testing.T) {
+	var tests = []struct {
+		name   string
+		input  []CustomType
+		output []CustomType
+	}{
+		{
+			"should filter out duplicates where duplicates are present",
+			[]CustomType{ct("first"), ct("second"), ct("third"), ct("second"), ct("first")},
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+		},
+		{
+			"should filter out duplicates where no duplicates are present",
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+			[]CustomType{ct("first"), ct("second"), ct("third")},
+		},
+	}
+
+	for _, test := range tests {
+		c := NewCustomTypeSlice(test.input)
 
 		got := c.Uniq()
 
